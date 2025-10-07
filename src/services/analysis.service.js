51a189285d63957a73c0a3177e800d8f24927803
kinjatoro <lucaslopezvazquez1@gmail.com>
2025-10-07 11:44:11 -0300

    update

--- src/services/analysis.service.js
+++ src/services/analysis.service.js
@@ -2,16 +2,16 @@
 
 import { connection } from "../../config/db.js";
 
-export const insertAnalysis = ({ usuario_id, estilo, tipo_error, duracion_video, observaciones }) => {
+export const insertAnalysis = ({ usuario_id, estilo, duracion_video, observaciones, csv, video }) => {
   return new Promise((resolve, reject) => {
     const query = `
       INSERT INTO historial_analisis 
-      (usuario_id, estilo, tipo_error, duracion_video, observaciones) 
-      VALUES (?, ?, ?, ?, ?)`;
+      (usuario_id, estilo, duracion_video, observaciones, csv, video) 
+      VALUES (?, ?, ?, ?, ?, ?)`;
 
     connection.query(
       query,
-      [usuario_id, estilo, tipo_error, duracion_video, observaciones],
+      [usuario_id, estilo, duracion_video, observaciones, csv, video ],
       (err, result) => {
         if (err) return reject(err);
         resolve(result);
