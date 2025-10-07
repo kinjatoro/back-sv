f2d7af62517c54046b07f3a9f610f223cea570e1
kinjatoro <lucaslopezvazquez1@gmail.com>
2025-10-07 17:32:51 -0300

    update

--- scripts/query.js
+++ scripts/query.js
@@ -1,9 +1,42 @@
 import { connection } from "../config/db.js";
 
+
+
+
 // Insertar registros solo en la columna `csv`
 const insertCsvColumn = `
-ALTER TABLE historial_analisis ADD COLUMN video TEXT;
+INSERT INTO correcciones (analisis_id, tipo_error, descripcion, snapshot_url)
+VALUES
+  (18, 'Ángulo de brazada muy alto.', 'El ángulo de brazada actual es demasiado grande (162°). Lo ideal es entre 45° y 60°. Recomendamos ejercicios drill con tabla unilateral.', 'https://swimvision-upload-videos.s3.us-east-1.amazonaws.com/outputs/vid4-error1.png'),
+  (18, 'Patada tipo bicicleta', 'La patada actualmente tiene un ángulo demasiado chico. Eso genera resistencia al agua y se pierde velocidad en el nado. Recomendamos ejercicios de patada con tabla.', 'https://swimvision-upload-videos.s3.us-east-1.amazonaws.com/outputs/vid4-error2.png')
 `;
+
+
+
+
+/*
+const insertCsvColumn = `
+DELETE FROM historial_analisis
+WHERE usuario_id = 3;
+`
+;
+*/
+
+
+
+
+/*
+const insertCsvColumn = `
+ALTER TABLE correcciones DROP FOREIGN KEY correcciones_ibfk_1;
+
+
+
+`*/
+
+
+
+
+
 
 connection.query(insertCsvColumn, (err) => {
   if (err) {
